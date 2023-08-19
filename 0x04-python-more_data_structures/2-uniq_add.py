6f02335fe241b11dd3e4d6ba3349cd9fbc38debc
root <ShamsTech005>
2023-08-19 04:39:11 +0000

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def uniq_sum(my_list=None):
-    if my_list is None:
-        my_list = []
-    return sum(set(my_list))
+def uniq_add(my_list=[]):
+    return(sum(set(my_list)))
